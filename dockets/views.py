8e5fa79f892818a5d7324af151b2ebd565e21ffb
dsinger <singerdw00@gmail.com>
2020-09-07 21:14:01 -0700

    initial django setup

---
+++ dockets/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+import json, os
+
+def index(request):
+    dirname = os.getcwd() + "/dockets/docket_data"
+
+    docket_data = []
+    for filename in os.listdir(dirname):
+        with open(os.path.join(dirname, filename), 'r') as f: # open in readonly mode
+            data=f.read()
+
+            obj = json.loads(data)
+            users.append(data)
+
+    return HttpResponse(f"Hello, world. You're at the dockets index. there are {len(docket_data)} files to display")
